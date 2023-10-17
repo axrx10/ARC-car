6714db714c4f8e4bd9143d14cc8ced10cb33d561
axrx10 <assermoemen6572gmail.com>
2023-10-17 20:30:25 +0100

    sensor works, place holder condition for now

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,46 +1,42 @@
-
-from RPi.GPIO import GPIO  
-
+#!/usr/bin/python
+import RPi.GPIO as GPIO
 import time
 
-class DistanceSensor:
-    def __init__(self, trig_pin, echo_pin):
-        self.trig_pin = trig_pin
-        self.echo_pin = echo_pin
+try:
+    GPIO.setmode(GPIO.BOARD)
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.trig_pin, GPIO.OUT)
-        GPIO.setup(self.echo_pin, GPIO.IN)
-        GPIO.output(self.trig_pin, False)
+    PIN_TRIGGER = 7
+    PIN_ECHO = 11
 
-    def read(self):
-        GPIO.output(self.trig_pin, True)
+    GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+    GPIO.setup(PIN_ECHO, GPIO.IN)
+
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+    print("Waiting for sensor to settle")
+    time.sleep(2)
+
+    print("Starting distance measurement")
+    
+    while True:
+        GPIO.output(PIN_TRIGGER, GPIO.HIGH)
         time.sleep(0.00001)
-        GPIO.output(self.trig_pin, False)
+        GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-        start_time = time.time()
-        stop_time = time.time()
+        while GPIO.input(PIN_ECHO) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(PIN_ECHO) == 1:
+            pulse_end_time = time.time()
 
-        while GPIO.input(self.echo_pin) == 0:
-            start_time = time.time()
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration * 17150, 2)
+        
+        if distance <= 20:
+            print("Car stopped moving")
+        else:
+            print("Distance:", distance, "cm")
 
-        while GPIO.input(self.echo_pin) == 1:
-            stop_time = time.time()
+        time.sleep(1)  # Add delay of 1 second before the next measurement
 
-        time_elapsed = stop_time - start_time
-        distance = (time_elapsed * 34300) / 2
-
-        return distance
-
-    def cleanup(self):
-        GPIO.cleanup()
-
-if __name__ == "__main__":
-    sensor = DistanceSensor(trig_pin=23, echo_pin=24)
-    try:
-        while True:
-            distance = sensor.read()
-            print(f"Distance: {distance} cm")
-            time.sleep(1)
-    except KeyboardInterrupt:
-        sensor.cleanup()
+finally:
+    GPIO.cleanup()
